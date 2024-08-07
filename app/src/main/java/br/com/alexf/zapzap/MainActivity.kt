b082eabbe5ac0d19e6772177eed3dcd91d4f0a78
Alex Felipe <alexfelipevieira@gmail.com>
2024-08-07 09:44:53 -0300

    adicionando primeira tela e top app bar

--- app/src/main/java/br/com/alexf/zapzap/MainActivity.kt
+++ app/src/main/java/br/com/alexf/zapzap/MainActivity.kt
@@ -4,26 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alexf.zapzap.ui.theme.ZapZapTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ZapZapTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    TopAppBar(title = {
+                        Text("Zap Zap")
+                    }, actions = {
+                        Row(Modifier.padding(8.dp),
+                            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                            Icon(Icons.Default.CameraAlt, contentDescription = null)
+                            Icon(Icons.Default.MoreVert, contentDescription = null)
+                        }
+                    })
+                }) { innerPadding ->
+                    ChatsListScreen(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +52,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZapZapTheme {
-        Greeting("Android")
+fun ChatsListScreen(modifier: Modifier = Modifier) {
+    Box(modifier.fillMaxSize()) {
+        Text(
+            "Chats List",
+            Modifier.align(Alignment.Center),
+            style = TextStyle.Default.copy(
+                fontSize = 32.sp
+            )
+        )
     }
 }
