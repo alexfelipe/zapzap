0bb880304749610cc818bda98a63fb30fc9747e4
Alex Felipe <alexfelipevieira@gmail.com>
2024-08-07 12:14:51 -0300

    Adicionando a top app bar

--- app/src/main/java/br/com/alexf/zapzap/MainActivity.kt
+++ app/src/main/java/br/com/alexf/zapzap/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.alexf.zapzap
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,16 +11,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Message
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.CircleNotifications
+import androidx.compose.material.icons.filled.Message
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,21 +46,79 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ZapZapTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-                    TopAppBar(title = {
-                        Text("Zap Zap")
-                    }, actions = {
-                        Row(Modifier.padding(8.dp),
-                            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                            Icon(Icons.Default.CameraAlt, contentDescription = null)
-                            Icon(Icons.Default.MoreVert, contentDescription = null)
+                App()
+            }
+        }
+    }
+
+}
+
+data class NavItem(
+    val icon: ImageVector,
+    val label: String
+)
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun App() {
+    val items = remember {
+        listOf(
+            NavItem(
+                icon = Icons.AutoMirrored.Filled.Message,
+                label = "Chats"
+            ),
+            NavItem(
+                icon = Icons.Default.CircleNotifications,
+                label = "Updates"
+            ),
+            NavItem(
+                icon = Icons.Default.People,
+                label = "Communities"
+            ),
+            NavItem(
+                icon = Icons.Default.Call,
+                label = "Calls"
+            )
+        )
+    }
+    var selectedItem by remember {
+        mutableStateOf(items.first())
+    }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = {
+                Text("Zap Zap")
+            }, actions = {
+                Row(
+                    Modifier.padding(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Icon(Icons.Default.CameraAlt, contentDescription = null)
+                    Icon(Icons.Default.MoreVert, contentDescription = null)
+                }
+            })
+        },
+        bottomBar = {
+            BottomAppBar {
+                items.forEach { navItem ->
+                    NavigationBarItem(
+                        selected = navItem == selectedItem,
+                        onClick = {
+                            selectedItem = navItem
+                        },
+                        icon = {
+                            Icon(navItem.icon, contentDescription = null)
+                        },
+                        label = {
+                            Text(navItem.label)
                         }
-                    })
-                }) { innerPadding ->
-                    ChatsListScreen(Modifier.padding(innerPadding))
+                    )
                 }
             }
         }
+    ) { innerPadding ->
+        ChatsListScreen(Modifier.padding(innerPadding))
     }
 }
 
@@ -61,5 +132,13 @@ fun ChatsListScreen(modifier: Modifier = Modifier) {
                 fontSize = 32.sp
             )
         )
+    }
+}
+
+@Preview
+@Composable
+private fun AppPreview() {
+    ZapZapTheme {
+        App()
     }
 }
